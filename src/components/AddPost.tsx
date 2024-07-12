cef0048270c50f4fbd548fdff2bab76ed7d5e938
Borys Ivliev <boris.ivlev.11@gmail.com>
2024-07-12 19:10:30 +0200

    The layout from the Figma layout was transferred to the application. Made side menus, profile page, main page and page elements such as posts, sponsors, etc.

---
+++ src/components/AddPost.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+import React from "react";
+
+const AddPost = () => {
+    return (
+        <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
+            <Image
+                src=""
+                alt=""
+                className="w-12 h-12 object-cover rounded-full"
+                width={48}
+                height={48}
+            />
+            <div className="flex-1">
+                <div className="flex gap-4">
+                    <textarea
+                        placeholder="What's on your mind?"
+                        className="flex-1 bg-slate-100 rounded-lg p-2"
+                    ></textarea>
+                    <Image
+                        src="/emoji.png"
+                        alt="Emoji"
+                        className="h-5 w-5 cursor-pointer self-end"
+                        width={20}
+                        height={20}
+                    />
+                </div>
+                <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
+                    <div className="flex items-center gap-2 cursor-pointer">
+                        <Image
+                            src="/addImage.png"
+                            alt="AddPhoto"
+                            width={20}
+                            height={20}
+                        />
+                        Photo
+                    </div>
+                    <div className="flex items-center gap-2 cursor-pointer">
+                        <Image
+                            src="/addVideo.png"
+                            alt="AddVideo"
+                            width={20}
+                            height={20}
+                        />
+                        Video
+                    </div>
+                    <div className="flex items-center gap-2 cursor-pointer">
+                        <Image
+                            src="/poll.png"
+                            alt="Poll"
+                            width={20}
+                            height={20}
+                        />
+                        Poll
+                    </div>
+                    <div className="flex items-center gap-2 cursor-pointer">
+                        <Image
+                            src="/addEvent.png"
+                            alt="AddEvent"
+                            width={20}
+                            height={20}
+                        />
+                        Event
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AddPost;
---
+++ src/components/Post.tsx
@@ -0,0 +1,86 @@
+import Image from "next/image";
+import React from "react";
+import Comments from "./Comments";
+
+const Post = () => {
+    return (
+        <div className="flex flex-col gap-4">
+            <div className="flex items-center justify-between">
+                <div className="flex items-center gap-4">
+                    <Image
+                        src=""
+                        alt=""
+                        className="w-10 h-10 rounded-full"
+                        width={40}
+                        height={40}
+                    />
+                    <span className="font-medium">Ricky</span>
+                </div>
+                <Image src="/more.png" alt="" width={16} height={16} />
+            </div>
+            <div className="flex flex-col gap-4">
+                <div className="w-full min-h-96 relative">
+                    <Image
+                        src=""
+                        alt=""
+                        fill
+                        className="object-cover rounded-md"
+                    />
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Eaque cupiditate fugit magnam, dolorum quasi veritatis
+                    voluptate dignissimos voluptas illo blanditiis doloremque
+                    fuga! Itaque laborum vero, nisi consequuntur nemo rem omnis?
+                </p>
+            </div>
+            <div className="flex items-center justify-between text-sm my-4">
+                <div className="flex gap-8">
+                    <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
+                        <Image
+                            src="/like.png"
+                            alt=""
+                            fill
+                            className="cursor-pointer"
+                        />
+                        <span className="text-gray-300">|</span>
+                        <span className="text-gray-500">
+                            123 <span className="hidden md:inline"> Likes</span>
+                        </span>
+                    </div>
+                    <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
+                        <Image
+                            src="/comment.png"
+                            alt=""
+                            fill
+                            className="cursor-pointer"
+                        />
+                        <span className="text-gray-300">|</span>
+                        <span className="text-gray-500">
+                            123{" "}
+                            <span className="hidden md:inline"> Comments</span>
+                        </span>
+                    </div>
+                </div>
+                <div className="">
+                    <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
+                        <Image
+                            src="/share.png"
+                            alt=""
+                            fill
+                            className="cursor-pointer"
+                        />
+                        <span className="text-gray-300">|</span>
+                        <span className="text-gray-500">
+                            123{" "}
+                            <span className="hidden md:inline"> Shares</span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <Comments />
+        </div>
+    );
+};
+
+export default Post;
